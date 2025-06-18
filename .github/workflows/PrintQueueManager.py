eab0ca1784fc6841efbe10e60061f864203b0541
fiza0 <issafuad05@gmail.com>
2025-06-18 21:17:10 +0300

    create printqueuemanager class and create it's constructor method

--- .github/workflows/PrintQueueManager.py
+++ .github/workflows/PrintQueueManager.py
@@ -10,3 +10,11 @@ class PrintJob:
     priority: int
     waiting_time: int = 0
     timestamp: int = 0
+class PrintQueueManager:
+    def __init__(self, capacity: int = 10):
+        self.capacity = capacity
+        self.queue: List[Optional[PrintJob]] = [None] * capacity
+        self.front = -1  # Index of front element
+        self.rear = -1   # Index of rear element
+        self.size = 0    # Current number of jobs in queue
+        self.lock = threading.Lock()  # For thread safety
